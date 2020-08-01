dd3b404e9812074080af4373c6b31b2f4416f8bf
mahmud <you@example.com>
2020-08-01 20:54:35 +0600

    modal added

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import useFireStore from '../hooks/useFireStore'
 
-export default function ImageGrid() {
+export default function ImageGrid({setSelectedImg}) {
     const {docs} = useFireStore('image')
     console.log(docs)
     return (
         <div className="img-grid">
             {docs && docs.map(doc =>(
-                <div className="img-wrap" key={doc.id}> 
+                <div className="img-wrap" key={doc.id} onClick={() => setSelectedImg(doc.url)}> 
                     <img src={doc.url} alt={doc.id}/>
                 </div>
             ))}
