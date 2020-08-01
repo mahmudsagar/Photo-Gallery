0a1227da498528a5c37c31b969429287f1718de1
mahmud <you@example.com>
2020-08-01 21:08:23 +0600

    added animation

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import useFireStore from '../hooks/useFireStore'
+import {motion} from 'framer-motion'
+
 
 export default function ImageGrid({setSelectedImg}) {
     const {docs} = useFireStore('image')
-    console.log(docs)
     return (
-        <div className="img-grid">
-            {docs && docs.map(doc =>(
-                <div className="img-wrap" key={doc.id} onClick={() => setSelectedImg(doc.url)}> 
-                    <img src={doc.url} alt={doc.id}/>
-                </div>
-            ))}
-        </div>
-    )
+      <div className="img-grid">
+        {docs &&
+          docs.map((doc) => (
+            <motion.div
+              className="img-wrap"
+              key={doc.id}
+              onClick={() => setSelectedImg(doc.url)}
+              whileHover={{ opacity: 1 }}
+              layout
+            >
+              <motion.img
+                src={doc.url}
+                alt="uploaded pic"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1 }}
+              />
+            </motion.div>
+          ))}
+      </div>
+    );
 }
